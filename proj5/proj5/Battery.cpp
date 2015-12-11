c8d4cc7cb06419cbd88fd0a83228dceb2dc17c2c
Alexander Wu <alex606@att.net>
2015-12-10 22:16:15 -0600

    Did Battery things

--- proj5/proj5/Battery.cpp
+++ proj5/proj5/Battery.cpp
@@ -5,7 +5,42 @@ Battery::Battery()
 {
 }
 
+Battery::Battery(string T, string C, int L, 
+	string O, string M, string D, int Q, float P)
+	: Part(O, M, D, Q, P)
+{
+	this->Type = T;
+	this->Cell = C;
+	this->Life = L;
+}
+
 
 Battery::~Battery()
 {
+}
+
+// Used Life and price as a means to quantify tire quality
+bool operator==(Battery& b1, Battery& b2)
+{
+	if (b1.Price == b2.Price && b1.Life == b2.Life)
+	{
+		return true;
+	}
+	else return false;
+}
+bool operator>(Battery& b1, Battery& b2)
+{
+	if (b1.Price <= b2.Price && b1.Life >= b2.Life)
+	{
+		return true;
+	}
+	else return false;
+}
+bool operator<(Battery& b1, Battery& b2)
+{
+	if (b1.Price >= b2.Price && b1.Life <= b2.Life)
+	{
+		return true;
+	}
+	else return false;
 }
--- proj5/proj5/Battery.h
+++ proj5/proj5/Battery.h
@@ -1,11 +1,24 @@
 #ifndef BATTERY_H
 #define BATTERY_H
 
-class Battery
+#include "Part.h"
+
+class Battery : public Part
 {
+private:
+	string Type;
+	string Cell;
+	int Life;
+
 public:
 	Battery();
+	Battery(string T, string C, int L, string oem, string manufacturer, string description, int quantity, float price);
 	~Battery();
+
+	friend bool operator== (Battery&, Battery&);
+	friend bool operator> (Battery&, Battery&);
+	friend bool operator< (Battery&, Battery&);
+
 };
 
 #endif
